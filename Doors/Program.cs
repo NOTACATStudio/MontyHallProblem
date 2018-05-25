cae8d58db8351eb7e427fedc2b890da12bcffe09
Chris <christopher.hull@usablelife.com>
2018-05-25 14:45:32 -0500

    fixing issue with live score updating

--- Doors/Program.cs
+++ Doors/Program.cs
@@ -17,7 +17,7 @@ namespace Doors
             var stopwatch = new Stopwatch();
             var choosy = new Choosy();
             var iterations = 1000000000;
-            var score = new Score(iterations);
+            var score = new Score();
             stopwatch.Start();
             for (int i = 0; i < iterations; i++)
             {
@@ -42,10 +42,7 @@ namespace Doors
 
         private static void PrintResults(Score score, Stopwatch stopwatch, int i)
         {
-            Console.WriteLine($"Number of correct guesses: {score.Wins}");
-            Console.WriteLine($"Number of incorrect guesses: {score.Losses}");
-            Console.WriteLine($"Percentage of wins: {score.Ratio}%");
-            Console.WriteLine("");
+            PrintScoreInfo(score);
             Console.WriteLine($"Iterations: {i}");
             Console.WriteLine($"Total Time Elapsed: {stopwatch.Elapsed.Hours} Hours {stopwatch.Elapsed.Minutes} Minutes {stopwatch.Elapsed.Seconds} Seconds");
         }
@@ -53,13 +50,18 @@ namespace Doors
         private static void PrintTime(Score score, Stopwatch stopwatch, int i, int iterations)
         {
             Console.Clear();
-            Console.WriteLine($"Number of correct guesses: {score.Wins}");
-            Console.WriteLine($"Number of incorrect guesses: {score.Losses}");
-            Console.WriteLine($"Percentage of wins: {score.Ratio}%");
-            Console.WriteLine("");
+            PrintScoreInfo(score);
             Console.WriteLine($"Time Elapsed: {stopwatch.Elapsed.Hours} Hours {stopwatch.Elapsed.Minutes} Minutes {stopwatch.Elapsed.Seconds} Seconds");
             Console.WriteLine($"Iterations: {i}");
             Console.WriteLine($"Percent Complete: {Math.Round(((decimal)i / iterations) * 100, 2)}%");
+        }
+
+        private static void PrintScoreInfo(Score score)
+        {
+            Console.WriteLine($"Number of correct guesses: {score.Wins}");
+            Console.WriteLine($"Number of incorrect guesses: {score.Losses}");
+            Console.WriteLine($"Percentage of wins: {score.Ratio}%"); //this doesn't work during the live update
+            Console.WriteLine("");
         }
     }
 }
--- Doors/Score.cs
+++ Doors/Score.cs
@@ -4,14 +4,8 @@ namespace Doors
 {
     public class Score
     {
-        public Score(int iterations)
-        {
-            Iterations = iterations;
-        }
-
-        public int Iterations { get; set; }
         public int Wins { get; set; } 
         public int Losses { get; set; }
-        public decimal Ratio => Losses == 0 ? 100 : Math.Round(((decimal)Wins/Iterations)*100, 2);
+        public decimal Ratio => Losses == 0 ? 100 : Math.Round((decimal)Wins/(Wins+Losses)*100, 2);
     }
 }
